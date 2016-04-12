a25784c0865bcf4fba6dbd28e68c125266d1ae68
haithem aouina <haouina09@gmail.com>
2016-04-12 20:27:13 +0100

    modif de code de webservice + ajout de fichier de config

--- webservice.py
+++ webservice.py
@@ -3,7 +3,8 @@
 import web
 import datetime
 import platform
-
+import config
+import pytz
 
 urls = (
     '/(.*)', 'time'
@@ -13,13 +14,15 @@ app = web.application(urls, globals())
 date = datetime.datetime.now()
 dist = platform.linux_distribution()[0]
 release = platform.linux_distribution()[1]
+timezone = pytz.timezone(config.Default_Timezone)
+date_modif = timezone.localize(date)
 
 
 class time:
     def GET(self, time):
         try:
-            if dist == 'Red Hat' and release == '6.0':
-                return 'The Time is: ' + str(date)
+            if dist == 'Ubuntu' and release == '14.04':
+                return 'The Time is: ' + str(date_modif)
         except ValueError:
             print 'Error'
         finally:
