fdf68129e486bcd1747555eb57cad89dfd9fd858
haithem aouina <haouina09@gmail.com>
2016-04-14 13:10:05 +0100

    Test de webservice + Mise à jour de Readme

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+
+import urllib
+import urllib2
+
+url = "http://192.168.1.26:8080/time"
+headers = {"Content-Type": "application/json", "Authorization": "Basic "}
+data = {"param": "value"}
+request = urllib2.Request(url)
+
+for key, value in headers.items():
+    request.add_header(key, value)
+
+response = urllib2.urlopen(request)
+
+print response.info().headers
+print response.read()
