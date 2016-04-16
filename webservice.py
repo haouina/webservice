ff47bd28c7ab1648cba480f62b5b3201b4432b82
haithem aouina <haouina09@gmail.com>
2016-04-16 13:29:40 +0100

    dernière modification

--- webservice.py
+++ webservice.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python2.7
 
 import web
-import datetime
 import platform
 import config
 import pytz
-import time 
+import time
+
 
 urls = (
     config.link, 'temps'
 )
 app = web.application(urls, globals())
-date = time.localtime()
 dist = platform.linux_distribution()[0]
 release = platform.linux_distribution()[1]
-
 
 
 class temps:
     def GET(self, temps):
         try:
             if dist == 'Ubuntu' and release == '14.04':
-                return time.strftime('Nous sommes: en %Y, et il est %H:%M:%S', time.localtime())
+                return time.strftime('Il est %H:%M:%S', time.localtime())
         except ValueError:
             print 'Error'
         finally:
