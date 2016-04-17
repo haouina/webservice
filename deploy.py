fac3713956ace1ecbb9a1b82c4c0b2ab1b87205f
haithem aouina <haouina09@gmail.com>
2016-04-17 20:56:01 +0100

    Modification de deploy.py

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
-from fabric.api import cd, env, prefix, run, task, prompt
+from fabric.api import cd, env, prefix, run, task, prompt, local, get
 
 env.hosts = ['prod']
+env.user   = "root"
 
 
 @task
@@ -21,3 +22,38 @@ def install(pkg=None):
     elif pkg is None and env.get("pkg") is None:
         env["pkg"] = prompt("Which package? ")
     run('apt-get install -y %s' % env["pkg"])
+
+
+@task
+def check():
+    run("mkdir /tmp/trunk/")
+    run("uptime")
+    run("hostname")
+    result = run("ls -l /var/www")
+    result.failed
+
+
+@task
+def local_tar():
+    local("tar -xzvf /root/web.py-0.37.tar.gz -C /root")    
+
+
+@task
+def file_get(remote_path, local_path):
+    get(remote_path, local_path)
+
+
+@task
+def update_upgrade():
+    run("aptitude    update")
+    run("aptitude -y upgrade")
+
+@task
+def install_memcached():
+    run("aptitude install -y memcached")
+
+
+@task
+def update_install():
+    update_upgrade()
+    install_memcached()
